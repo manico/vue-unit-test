9bfde6e51460e95f014c9dbe2910e714e80f95fc
Jadranko Dragoje <dragoje.jadranko@nsoft.com>
2018-12-05 22:25:06 +0100

    Add emitter

---
+++ tests/unit/OutcomeButton-4.spec.js
@@ -0,0 +1,31 @@
+import { shallowMount } from '@vue/test-utils';
+import OutcomeButton from '@/components/OutcomeButton.vue';
+
+describe('OutcomeButton.vue', () => {
+
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallowMount(OutcomeButton, {
+      propsData: {
+        id: '10/250/4000',
+        label: 'Manchester City',
+        odd: 1.45,
+      },
+    });
+  });
+
+  it('selected event is emitted', () => {
+    wrapper.trigger('click');
+    expect(wrapper.emitted().selected).toBeTruthy();
+  });
+
+  it('selected event is emitted with correct payload', () => {
+    wrapper.trigger('click');
+    expect(wrapper.emitted().selected[0][0]).toEqual({
+      id: '10/250/4000',
+      label: 'Manchester City',
+      odd: 1.45,
+      previousOdd: null,
+    });
+  });
+});
