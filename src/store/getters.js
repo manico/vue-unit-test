080d1a2b9603ebdba32942f62ddd186811aa1108
Jadranko Dragoje <dragoje.jadranko@nsoft.com>
2018-12-06 11:37:23 +0100

    Add new getter

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,6 +1,11 @@
+import { filter } from 'lodash';
+
 export default {
   matches(state) {
     return state.matches;
+  },
+  activeMatches(state) {
+    return filter(state.matches, { active: true });
   },
   user(state) {
     return state.user;
